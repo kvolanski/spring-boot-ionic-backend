b55aebacf3a52369999c21bba69ed7dc71fec9ad
Kevin Volanski <kvolanski@gmail.com>
2020-04-24 22:21:30 -0300

    Implementando autorizacao

--- src/main/java/com/kevinvolanski/cursomc/config/SecurityConfig.java
+++ src/main/java/com/kevinvolanski/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.kevinvolanski.cursomc.security.JWTAuthenticationFilter;
+import com.kevinvolanski.cursomc.security.JWTAuthorizationFilter;
 import com.kevinvolanski.cursomc.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
