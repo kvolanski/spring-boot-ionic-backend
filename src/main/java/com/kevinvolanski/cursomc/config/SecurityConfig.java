4643e504bd33cace6393592dba6204cedee7218d
Kevin Volanski <kvolanski@gmail.com>
2020-05-07 20:50:33 -0300

    Liberando CORS para PUT e DELETE

--- src/main/java/com/kevinvolanski/cursomc/config/SecurityConfig.java
+++ src/main/java/com/kevinvolanski/cursomc/config/SecurityConfig.java
@@ -71,8 +71,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+		corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", corsConfiguration);
 		return source;
 	}
 
